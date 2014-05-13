e0054139254c121d935a7c63ae57eb782881e19b
WangHao <bingoogol@sina.com>
2014-05-13 22:59:49 +0800

    修改毕业论文

---
+++ 资料/文档/王浩-终期/维普检测报告/script\reportshow.js
@@ -0,0 +1,7 @@
+﻿$(function(){
+	$('.cqvip-jianceorgan-paperreportpart').click(function(){
+		var partid=$(this).attr('partid');
+		var parthtml=$('#report_part_'+partid).html();
+		$('.cqvip-jianceorgan-paperreportpartpanel').html(parthtml);
+	});
+});
