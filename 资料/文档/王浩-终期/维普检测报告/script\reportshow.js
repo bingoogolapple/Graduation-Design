c63c43c09c86a5ccd20626ecab0bc10ef875bdba
WangHao <bingoogol@sina.com>
2014-05-21 08:59:51 +0800

    删除多余的检测文档

--- 资料/文档/王浩-终期/维普检测报告/script\reportshow.js
+++
@@ -1,7 +0,0 @@
-﻿$(function(){
-	$('.cqvip-jianceorgan-paperreportpart').click(function(){
-		var partid=$(this).attr('partid');
-		var parthtml=$('#report_part_'+partid).html();
-		$('.cqvip-jianceorgan-paperreportpartpanel').html(parthtml);
-	});
-});
